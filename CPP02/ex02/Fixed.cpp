f91edf7d8340441f61f0cc699c650207126f6e10
maskface02 <your-email@example.com>
2025-12-18 16:01:30 +0100

    .

---
+++ CPP02/ex02/Fixed.cpp
@@ -0,0 +1,130 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                                            */
+/*   Fixed.cpp                                            :::      ::::::::   */
+/*                                                      :+:      :+:    :+:   */
+/*   By: zatais <zatais@student.1337.ma>              +:+ +:+         +:+     */
+/*                                                  +#+  +:+       +#+        */
+/*   Created: 2025/12/17 23:14:51 by zatais       +#+#+#+#+#+   +#+           */
+/*   Updated: 2025/12/17 23:14:51 by zatais            #+#    #+#             */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+Fixed::Fixed() : fixedPointVal(0) {}
+
+Fixed::Fixed(const Fixed& obj){
+  *this = obj;
+}
+
+Fixed::Fixed(const int intValue) {
+  fixedPointVal = intValue << fractionBits;
+}
+
+Fixed::Fixed(const float floatValue) {
+  fixedPointVal = roundf(floatValue * (1 << fractionBits));
+}
+
+Fixed::~Fixed(){}
+
+float Fixed::toFloat() const {
+  return (float)fixedPointVal / (1 << fractionBits);
+}
+
+int Fixed::toInt() const {
+  return fixedPointVal >> fractionBits;
+}
+
+int Fixed::getRawBits() const {
+  return fixedPointVal;
+}
+
+void Fixed::setRawBits(int const raw) {
+  fixedPointVal = raw;
+}
+
+std::ostream& operator<<(std::ostream& out, const Fixed& fixed) {
+  out << fixed.toFloat();
+  return out;
+}
+
+/*-------------------------------------------------------------------------------*/
+
+Fixed& Fixed::operator=(const Fixed& obj){
+  if (this != &obj)
+    fixedPointVal = obj.getRawBits();
+  return *this;
+}
+
+Fixed Fixed::operator*(const Fixed &obj){
+  Fixed res;
+
+  res.setRawBits(fixedPointVal * obj.getRawBits());
+  return res;
+}
+
+Fixed Fixed::operator+(const Fixed &obj){
+  Fixed res;
+
+  res.setRawBits(fixedPointVal + obj.getRawBits());
+  return res;
+}
+
+Fixed Fixed::operator-(const Fixed &obj){
+  Fixed res;
+
+  res.setRawBits(fixedPointVal - obj.getRawBits());
+  return res;
+}
+
+Fixed Fixed::operator/(const Fixed &obj){
+  Fixed res;
+
+  res.setRawBits(fixedPointVal / obj.getRawBits());
+  return res;
+}
+
+/*--------------------------------------------------------------------------------*/
+
+bool Fixed::operator>(const Fixed &obj){return fixedPointVal > obj.getRawBits();}
+
+bool Fixed::operator<(const Fixed &obj){return fixedPointVal < obj.getRawBits();}
+
+bool Fixed::operator>=(const Fixed &obj){return fixedPointVal >= obj.getRawBits();}
+
+bool Fixed::operator<=(const Fixed &obj){return fixedPointVal <= obj.getRawBits();}
+
+bool Fixed::operator==(const Fixed &obj){return fixedPointVal == obj.getRawBits();}
+
+bool Fixed::operator!=(const Fixed &obj){return fixedPointVal != obj.getRawBits();}
+
+/*------------------------------------------------------------------------------------*/
+
+Fixed& Fixed::operator++(){
+  ++fixedPointVal;
+  return *this;
+}
+
+Fixed Fixed::operator++(int){
+  Fixed tmp = *this;
+
+  ++fixedPointVal;
+  return tmp;
+}
+
+Fixed& Fixed::operator--(){
+  --fixedPointVal;
+  return *this;
+}
+
+Fixed Fixed::operator--(int){
+  Fixed tmp = *this;
+
+  --fixedPointVal;
+  return tmp;
+}
+
+/*-----------------------------------------------------------------------------------*/
+
+
---
+++ CPP02/ex02/Fixed.hpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                                            */
+/*   Fixed.hpp                                            :::      ::::::::   */
+/*                                                      :+:      :+:    :+:   */
+/*   By: zatais <zatais@student.1337.ma>              +:+ +:+         +:+     */
+/*                                                  +#+  +:+       +#+        */
+/*   Created: 2025/12/17 22:48:31 by zatais       +#+#+#+#+#+   +#+           */
+/*   Updated: 2025/12/18 13:14:29 by zatais            #+#    #+#             */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed{
+  private:
+    int fixedPointVal;
+    static const int fractionBits = 8;
+
+  public:
+    Fixed();
+    Fixed(const int val);
+    Fixed(const float val);
+    Fixed(const Fixed &obj);
+
+    Fixed& operator=(const Fixed &obj);
+    Fixed  operator*(const Fixed &obj);
+    Fixed  operator+(const Fixed &obj);
+    Fixed  operator-(const Fixed &obj);
+    Fixed  operator/(const Fixed &obj);
+
+    bool   operator>(const Fixed &obj);
+    bool   operator<(const Fixed &obj);
+    bool   operator>=(const Fixed &obj);
+    bool   operator<=(const Fixed &obj);
+    bool   operator==(const Fixed &obj);
+    bool   operator!=(const Fixed &obj);
+
+    Fixed& operator++();
+    Fixed operator++(int);
+    Fixed& operator--();
+    Fixed operator--(int);
+    ~Fixed();
+
+    int toInt( void ) const;
+    float toFloat( void ) const;
+    int getRawBits( void ) const;
+    void setRawBits( int const raw );
+};
+
+std::ostream&	operator<<(std::ostream &out, Fixed const &obj);
+
+#endif
