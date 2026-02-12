f86e08ae993136013df9aebcaa8d3625f275bdd6
maskface02 <maskface02>
2026-02-12 16:27:03 +0100

    .

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: zatais <zatais@student.1337.ma>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/11 20:03:40 by zatais            #+#    #+#             */
-/*   Updated: 2026/02/11 22:38:24 by zatais           ###   ########.fr       */
+/*   Updated: 2026/02/12 16:04:04 by zatais           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef MUTANTSTACK
@@ -18,12 +18,48 @@ template <typename T> class MutantStack : public std::stack<T> {
 public:
   MutantStack();
   virtual ~MutantStack();
-  MutantStack(MutantStack const &);
-  MutantStack &operator=(MutantStack const &);
+  MutantStack(const MutantStack<T> &otherOBj);
+  MutantStack<T> &operator=(const MutantStack<T> &otherOBj);
 
-  typedef typename std::stack<T>::containter_type::iterator iterator;
+  typedef typename std::stack<T>::container_type::iterator iterator;
+  typedef typename std::stack<T>::container_type::const_iterator const_iterator;
   iterator begin();
+  const_iterator begin() const;
   iterator end();
+  const_iterator end() const;
 };
+
+template <typename T> MutantStack<T>::MutantStack() {}
+
+template <typename T> MutantStack<T>::~MutantStack() {}
+
+template <typename T>
+MutantStack<T>::MutantStack(const MutantStack<T> &otherOBj)
+    : std::stack<T>(otherOBj) {}
+
+template <typename T>
+MutantStack<T> &MutantStack<T>::operator=(const MutantStack<T> &otherOBj) {
+  std::stack<T>::operator=(otherOBj);
+  return *this;
+}
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::begin() {
+  return std::stack<T>::c.begin();
+}
+
+template <typename T>
+typename MutantStack<T>::const_iterator MutantStack<T>::begin() const {
+  return std::stack<T>::c.begin();
+}
+
+template <typename T> typename MutantStack<T>::iterator MutantStack<T>::end() {
+  return std::stack<T>::c.end();
+}
+
+template <typename T>
+typename MutantStack<T>::const_iterator MutantStack<T>::end() const {
+  return std::stack<T>::c.end();
+}
 
 #endif // !MutantStack
--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -9,6 +9,7 @@
 /*   Updated: 2026/02/11 21:36:58 by zatais           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include <iostream>
 #include "MutantStack.hpp"
 int main()
@@ -22,17 +23,23 @@ int main()
   mstack.push(3);
   mstack.push(5);
   mstack.push(737);
-  //[...]
   mstack.push(0);
+
   MutantStack<int>::iterator it = mstack.begin();
   MutantStack<int>::iterator ite = mstack.end();
   ++it;
   --it;
+  MutantStack<int>::const_iterator cit = mstack.begin();
+  ++cit;
+  --cit;
   while (it != ite)
   { 
-    std::cout << *it << std::endl;
+    std::cout << "normal "<< *it << std::endl;
+    std::cout << "const "<< *cit << std::endl;
+    mstack.pop();
     ++it;
+    ++cit;
   }
-  std::stack<int> s(mstack);
+ // *cit = 10; 
   return 0;
 }
